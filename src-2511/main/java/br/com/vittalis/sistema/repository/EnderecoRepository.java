7210d6cd81a27da513347097a55f586bb3204859
lauramedeirosr <lauramedeirosrr@gmail.com>
2025-12-02 12:26:04 -0300

    Merge remote-tracking branch 'origin/main'

--- src-2511/main/java/br/com/vittalis/sistema/repository/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.vittalis.sistema.repository;
-
-import br.com.vittalis.sistema.model.Endereco;
-import br.com.vittalis.sistema.model.Navio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
--- src-2511/main/java/br/com/vittalis/sistema/repository/TelefoneRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.vittalis.sistema.repository;
-
-import br.com.vittalis.sistema.model.Telefone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TelefoneRepository  extends  JpaRepository<Telefone, Long> {
-    Telefone findTelefoneByDddAndNumero(String ddd, String numero);
-}
